9d4f98ae2c5bdb24795d9a07313d58b661b4e14f
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2017-04-03 22:10:26 +0200

    no message

--- src/arboretum_speed_test.py
+++ src/arboretum_speed_test.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
                          'tree':
                              {
                                  'eta': 0.1,
-                                 'max_depth': 12,
+                                 'max_depth': 13,
                                  'gamma': 0.0,
                                  'min_child_weight': 100.0,
                                  'min_leaf_size': 0,
