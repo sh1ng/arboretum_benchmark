fff232b046421b53120fee1f95ccc0873f27b27f
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-02-21 15:34:55 +0100

    xgboost early stop

--- src/criteo_speed_test.py
+++ src/criteo_speed_test.py
@@ -9,7 +9,7 @@ def read_data():
     data = np.load('data/day_0.npz')
     return data['labels'], data['data_float'], data['data_cat']
 
-def run_lightgbm(label, data_float, data_cat, data_float_test, data_cat_test):
+def run_lightgbm(label, data_float, data_cat, label_test, data_float_test, data_cat_test):
     import lightgbm as lgb
 
     categorical_feature = list([i + data_float.shape[1] for i in range(data_cat.shape[1])])
@@ -41,7 +41,7 @@ def run_lightgbm(label, data_float, data_cat, data_float_test, data_cat_test):
 
     return gbm.predict(data_raw), gbm.predict(data_test)
 
-def run_arboretum(label, data_float, data_cat, data_float_test, data_cat_test):
+def run_arboretum(label, data_float, data_cat, label_test, data_float_test, data_cat_test):
     import arboretum
     import json
 
@@ -59,9 +59,9 @@ def run_arboretum(label, data_float, data_cat, data_float_test, data_cat_test):
         'tree':
         {
         'eta': 0.1,
-        'max_depth': 11,
+        'max_depth': 10,
         'gamma': 0.0,
-        'min_child_weight': 10,
+        'min_child_weight': 8,
         'min_leaf_size': 0,
         'colsample_bytree': 0.8,
         'colsample_bylevel': 0.8,
@@ -84,20 +84,20 @@ def run_arboretum(label, data_float, data_cat, data_float_test, data_cat_test):
 
     return model.predict(data), model.predict(data_test)
 
-def run_xgboost(label, data_float, data_cat, data_float_test, data_cat_test):
+def run_xgboost(label, data_float, data_cat, label_test, data_float_test, data_cat_test):
     import xgboost
 
     data_raw = np.concatenate([data_float, data_cat], axis=1)
     data_test = np.concatenate([data_float_test, data_cat_test], axis=1)
 
     data = xgboost.DMatrix(data_raw, label=label)
-    data_test = xgboost.DMatrix(data_test)
+    data_test = xgboost.DMatrix(data_test, label=label_test)
 
     param = {'max_depth': 9,
              'silent': False, 
              'objective': "reg:logistic"}
     param['nthread'] = 4
-    param['min_child_weight'] = 5
+    param['min_child_weight'] = 8
     param['colspan_by_tree'] = 0.8
     param['colspan_by_level'] = 0.8
     param['lambda'] = 0.1
@@ -105,10 +105,11 @@ def run_xgboost(label, data_float, data_cat, data_float_test, data_cat_test):
     param['gamma'] = 0.0
     param['alpha'] = 0.0
     param['tree_method'] = 'gpu_hist'
+    param['eval_metric'] = 'auc'
     
     start_time = time.time()
-
-    model = xgboost.train(param, data, 500)
+    watchlist  = [ (data,'train'),(data_test,'eval')]
+    model = xgboost.train(param, data, 5000, early_stopping_rounds=50, evals=watchlist)
 
     print(time.time() - start_time)
 
@@ -135,6 +136,6 @@ if __name__ == '__main__':
     print('startring benchmark {0}'.format(args.target))
 
     
-    prediction_train, prediction_test = benchmarks[args.target](label_train, data_float_train, data_cat_train, data_float_test, data_cat_test)
+    prediction_train, prediction_test = benchmarks[args.target](label_train, data_float_train, data_cat_train, label_test, data_float_test, data_cat_test)
 
     print('roc auc train:{0} cv:{1}'.format(roc_auc_score(label_train, prediction_train), roc_auc_score(label_test, prediction_test)))
