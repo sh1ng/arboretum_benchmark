5074bcb8425cdaffaec9b92b48db43f9a21b6826
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-05-19 14:51:35 +0200

    save data to pkl

--- src/criteo_data_prepare.py
+++ src/criteo_data_prepare.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def read_data():
+def read_data(idx):
     integer_features = list(['i{0}'.format(i) for i in range(1, 14, 1)])
     cat_features = list(['c{0}'.format(i) for i in range(1, 27, 1)])
     names = list(['label'])
@@ -16,18 +16,15 @@ def read_data():
     for item in cat_features:
         dtypes[item] = 'category'
 
-    data = pd.read_csv('data/day_0.gz', nrows=10000000,
+    data = pd.read_csv('data/day_{0}.gz'.format(idx), nrows=100000,
                        sep='\t', header=None, names=names, dtype=dtypes)
 
     print('reading is done....')
 
-    tmp_data = data[integer_features].values.astype(np.float32)
-    tmp_data = np.where(np.isnan(tmp_data), 0, tmp_data)
-    return data.label.values.astype(np.float32), tmp_data, data[cat_features].apply(lambda x: x.cat.codes).values.astype(np.int32)
+    return data
 
 
 if __name__ == '__main__':
-    labels, data_float, data_cat = read_data()
-    np.savez_compressed('data/day_0_small.npz', labels=labels,
-                        data_float=data_float, data_cat=data_cat)
+    for i in range(7):
+        read_data(i).to_pickle('data/day_{0}_pkl.gz'.format(i))
     print('saved')
