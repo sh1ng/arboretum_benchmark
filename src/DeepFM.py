8fa534c508bbb8e9024bebee7e1c0d4cc1d29cb7
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-05-28 15:40:39 +0200

    initial std

--- src/DeepFM.py
+++ src/DeepFM.py
@@ -6,6 +6,137 @@ from tensorflow.python.keras.initializers import Zeros, glorot_normal
 from tensorflow.python.keras.layers import Layer, Activation
 from tensorflow.python.keras.regularizers import l2
 
+class CIN(Layer):
+    """Compressed Interaction Network used in xDeepFM.This implemention is
+    adapted from code that the author of the paper published on https://github.com/Leavingseason/xDeepFM.
+
+      Input shape
+        - 3D tensor with shape: ``(batch_size,field_size,embedding_size)``.
+
+      Output shape
+        - 2D tensor with shape: ``(batch_size, featuremap_num)`` ``featuremap_num =  sum(self.layer_size[:-1]) // 2 + self.layer_size[-1]`` if ``split_half=True``,else  ``sum(layer_size)`` .
+
+      Arguments
+        - **layer_size** : list of int.Feature maps in each layer.
+
+        - **activation** : activation function used on feature maps.
+
+        - **split_half** : bool.if set to False, half of the feature maps in each hidden will connect to output unit.
+
+        - **seed** : A Python integer to use as random seed.
+
+      References
+        - [Lian J, Zhou X, Zhang F, et al. xDeepFM: Combining Explicit and Implicit Feature Interactions for Recommender Systems[J]. arXiv preprint arXiv:1803.05170, 2018.] (https://arxiv.org/pdf/1803.05170.pdf)
+    """
+
+    def __init__(self, layer_size=(128, 128), activation='relu', split_half=True, l2_reg=1e-5, seed=1024, **kwargs):
+        if len(layer_size) == 0:
+            raise ValueError(
+                "layer_size must be a list(tuple) of length greater than 1")
+        self.layer_size = layer_size
+        self.split_half = split_half
+        self.activation = activation
+        self.l2_reg = l2_reg
+        self.seed = seed
+        super(CIN, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        if len(input_shape) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (len(input_shape)))
+
+        self.field_nums = [input_shape[1].value]
+        self.filters = []
+        self.bias = []
+        for i, size in enumerate(self.layer_size):
+
+            self.filters.append(self.add_weight(name='filter' + str(i),
+                                                shape=[1, self.field_nums[-1]
+                                                       * self.field_nums[0], size],
+                                                dtype=tf.float32, initializer=glorot_uniform(seed=self.seed + i),
+                                                regularizer=l2(self.l2_reg)))
+
+            self.bias.append(self.add_weight(name='bias' + str(i), shape=[size], dtype=tf.float32,
+                                             initializer=tf.keras.initializers.Zeros()))
+
+            if self.split_half:
+                if i != len(self.layer_size) - 1 and size % 2 > 0:
+                    raise ValueError(
+                        "layer_size must be even number except for the last layer when split_half=True")
+
+                self.field_nums.append(size // 2)
+            else:
+                self.field_nums.append(size)
+
+        self.activation_layers = [activation_layer(self.activation) for _ in self.layer_size]
+
+        super(CIN, self).build(input_shape)  # Be sure to call this somewhere!
+
+    def call(self, inputs, **kwargs):
+
+        if K.ndim(inputs) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (K.ndim(inputs)))
+
+        dim = inputs.get_shape()[-1].value
+        hidden_nn_layers = [inputs]
+        final_result = []
+
+        split_tensor0 = tf.split(hidden_nn_layers[0], dim * [1], 2)
+        for idx, layer_size in enumerate(self.layer_size):
+            split_tensor = tf.split(hidden_nn_layers[-1], dim * [1], 2)
+
+            dot_result_m = tf.matmul(
+                split_tensor0, split_tensor, transpose_b=True)
+
+            dot_result_o = tf.reshape(
+                dot_result_m, shape=[dim, -1, self.field_nums[0] * self.field_nums[idx]])
+
+            dot_result = tf.transpose(dot_result_o, perm=[1, 0, 2])
+
+            curr_out = tf.nn.conv1d(
+                dot_result, filters=self.filters[idx], stride=1, padding='VALID')
+
+            curr_out = tf.nn.bias_add(curr_out, self.bias[idx])
+
+            curr_out = self.activation_layers[idx](curr_out)
+
+            curr_out = tf.transpose(curr_out, perm=[0, 2, 1])
+
+            if self.split_half:
+                if idx != len(self.layer_size) - 1:
+                    next_hidden, direct_connect = tf.split(
+                        curr_out, 2 * [layer_size // 2], 1)
+                else:
+                    direct_connect = curr_out
+                    next_hidden = 0
+            else:
+                direct_connect = curr_out
+                next_hidden = curr_out
+
+            final_result.append(direct_connect)
+            hidden_nn_layers.append(next_hidden)
+
+        result = tf.concat(final_result, axis=1)
+        result = tf.reduce_sum(result, -1, keep_dims=False)
+
+        return result
+
+    def compute_output_shape(self, input_shape):
+        if self.split_half:
+            featuremap_num = sum(
+                self.layer_size[:-1]) // 2 + self.layer_size[-1]
+        else:
+            featuremap_num = sum(self.layer_size)
+        return (None, featuremap_num)
+
+    def get_config(self, ):
+
+        config = {'layer_size': self.layer_size, 'split_half': self.split_half, 'activation': self.activation,
+                  'seed': self.seed}
+        base_config = super(CIN, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
 
 class DNN(Layer):
     """The Multi Layer Percetron
@@ -108,7 +239,7 @@ class FM(Layer):
 
 class DeepFM:
     def __init__(self, numerical_features, cat_features, embedding_size, dnn_size=(128, 128), dnn_activation=keras.activations.relu, l2_reg_dnn=0.0,
-                 l2_embedding=1e-4, seed=0, init_std=1.0):
+                 l2_embedding=1e-4, seed=0, init_std=0.01):
         self.dnn_size=dnn_size
         self.l2_reg_dnn =l2_reg_dnn
         self.l2_reg_embedding = l2_embedding
