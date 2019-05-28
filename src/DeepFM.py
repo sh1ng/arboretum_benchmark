5d8897b25ec906ea48f46b131916d79dbfbe349d
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-05-28 11:26:44 +0200

    deepFM

---
+++ src/DeepFM.py
@@ -0,0 +1,159 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+import tensorflow as tf
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.initializers import Zeros, glorot_normal
+from tensorflow.python.keras.layers import Layer
+from tensorflow.python.keras.regularizers import l2
+
+
+class DNN(Layer):
+    """The Multi Layer Percetron
+
+      Input shape
+        - nD tensor with shape: ``(batch_size, ..., input_dim)``. The most common situation would be a 2D input with shape ``(batch_size, input_dim)``.
+
+      Output shape
+        - nD tensor with shape: ``(batch_size, ..., hidden_size[-1])``. For instance, for a 2D input with shape ``(batch_size, input_dim)``, the output would have shape ``(batch_size, hidden_size[-1])``.
+
+      Arguments
+        - **hidden_units**:list of positive integer, the layer number and units in each layer.
+
+        - **activation**: Activation function to use.
+
+        - **l2_reg**: float between 0 and 1. L2 regularizer strength applied to the kernel weights matrix and bias.
+    """
+
+    def __init__(self, hidden_units, activation, l2_reg=0.0, **kwargs):
+        assert len(hidden_units) > 0
+        self.hidden_units = hidden_units
+        self.activation = activation
+        self.l2_reg = l2_reg
+        super(DNN, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.layers = [keras.layers.Dense(units, activation=self.activation, kernel_regularizer=keras.regularizers.l2(self.l2_reg),
+                                   bias_regularizer=keras.regularizers.l2(self.l2_reg)) for units in self.hidden_units]
+        super(DNN, self).build(input_shape)  # Be sure to call this somewhere!
+
+    def call(self, inputs, training=None, **kwargs):
+
+        deep_input = inputs
+
+        for layer in self.layers:
+            deep_input = layer(deep_input)
+
+        return deep_input
+
+    def compute_output_shape(self, input_shape):
+        if len(self.hidden_units) > 0:
+            shape = input_shape[:-1] + (self.hidden_units[-1],)
+        else:
+            shape = input_shape
+
+        return tuple(shape)
+
+    def get_config(self, ):
+        config = {'activation': self.activation, 'hidden_units': self.hidden_units,
+                  'l2_reg': self.l2_reg}
+        base_config = super(DNN, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+class FM(Layer):
+    """Factorization Machine models pairwise (order-2) feature interactions
+     without linear term and bias.
+
+      Input shape
+        - 3D tensor with shape: ``(batch_size,field_size,embedding_size)``.
+
+      Output shape
+        - 2D tensor with shape: ``(batch_size, 1)``.
+
+      References
+        - [Factorization Machines](https://www.csie.ntu.edu.tw/~b97053/paper/Rendle2010FM.pdf)
+    """
+
+    def __init__(self, **kwargs):
+
+        super(FM, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        if len(input_shape) != 3:
+            raise ValueError("Unexpected inputs dimensions % d,\
+                             expect to be 3 dimensions" % (len(input_shape)))
+
+        super(FM, self).build(input_shape)  # Be sure to call this somewhere!
+
+    def call(self, inputs, **kwargs):
+
+        if K.ndim(inputs) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions"
+                % (K.ndim(inputs)))
+
+        concated_embeds_value = inputs
+
+        square_of_sum = tf.square(tf.reduce_sum(
+            concated_embeds_value, axis=1, keep_dims=True))
+        sum_of_square = tf.reduce_sum(
+            concated_embeds_value * concated_embeds_value, axis=1, keep_dims=True)
+        cross_term = square_of_sum - sum_of_square
+        cross_term = 0.5 * tf.reduce_sum(cross_term, axis=2, keep_dims=False)
+
+        return cross_term
+
+    def compute_output_shape(self, input_shape):
+        return (None, 1)
+
+
+class DeepFM:
+    def __init__(self, numerical_features, cat_features, embedding_size, dnn_size=(128, 128), dnn_activation=keras.activations.relu, l2_reg_dnn=0.0,
+                 l2_embedding=1e-4, seed=0, init_std=1.0):
+        self.dnn_size=dnn_size
+        self.l2_reg_dnn =l2_reg_dnn
+        self.l2_reg_embedding = l2_embedding
+        self.embedding_size = embedding_size
+
+        numerical_input = list(map(lambda x: keras.layers.Input(shape=(1,), name='numerical_{0}'.format(x), dtype=tf.float32), numerical_features))
+        cat_input = list(map(lambda x: keras.layers.Input(shape=(1,), name="cat_{0}".format(x[0]), dtype=tf.int32), cat_features))
+
+        embeding_input = []
+        for idx, [name, size] in enumerate(cat_features):
+            embedding_layer = tf.keras.layers.Embedding(size, embedding_size,
+                                                  name='emb_' + name,
+                                                        embeddings_initializer=keras.initializers.RandomNormal(
+                                                            mean=0.0, stddev=init_std, seed=seed),
+                                                        embeddings_regularizer=keras.regularizers.l2(l2_embedding),
+                                                        )
+            embeding = embedding_layer(cat_input[idx])
+            embeding_input.append(embeding)
+
+        for idx, name in enumerate(numerical_features):
+            x = keras.layers.Dense(embedding_size, kernel_regularizer=keras.regularizers.l2(l2_embedding),
+                                   bias_regularizer=keras.regularizers.l2(l2_embedding))(numerical_input[idx])
+            x = keras.layers.Reshape([1, embedding_size])(x)
+            embeding_input.append(x)
+
+        concat = keras.layers.Concatenate(axis=1)(embeding_input)
+
+        fm_out = FM()(concat)
+        deep_input = tf.keras.layers.Flatten()(concat)
+
+        self.dnn = DNN(dnn_size, dnn_activation, l2_reg_dnn)
+
+        deep_out = self.dnn(deep_input)
+        deep_logit = tf.keras.layers.Dense(
+            1, use_bias=False, activation=None)(deep_out)
+
+        logit = keras.layers.add([deep_logit, fm_out])
+
+        self.keras_model = tf.keras.models.Model(cat_input + numerical_input, outputs=logit)
+
+    def model_identity(self):
+        name = 'DNN_emb_{0}_l2_emb_{1}_l2_dnn_{2}_dnn'.format(self.embedding_size, self.l2_reg_embedding, self.l2_reg_embedding)
+        for l in self.dnn_size:
+            name += "_{0}".format(l)
+
+        return name
+
+
---
+++ src/train_deepFM.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from sklearn.metrics import log_loss, roc_auc_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard
+
+from src.DeepFM import DeepFM
+
+if __name__ == "__main__":
+
+    cat_feature_names = ['C' + str(i) for i in range(1, 27)]
+    numerical_feature_names = ['I' + str(i) for i in range(1, 14)]
+
+    names = list(['label'])
+    names.extend(numerical_feature_names)
+    names.extend(cat_feature_names)
+
+    dtypes = {
+        'label': np.float32,
+    }
+    for item in numerical_feature_names:
+        dtypes[item] = np.float32
+    for item in cat_feature_names:
+        dtypes[item] = 'category'
+
+    file = '../../arboretum_benchmark/data/dac/train.txt'
+
+    data = pd.read_csv(file,
+                       sep='\t', header=None, names=names, dtype=dtypes, nrows=100000)
+
+    cat_features = []
+    threshold = 5
+    for col in cat_feature_names:
+        u, inverse, count = np.unique(data[col].cat.codes, return_counts=True, return_inverse=True)
+        original = len(u)
+        u[count < threshold] = len(u)
+        data[col] = u[inverse]
+        u, inverse = np.unique(data[col], return_inverse=True)
+        data[col] = inverse.astype(np.int32)
+
+        cat_features.append([col, len(u)])
+
+        print('removed low freq<{0} categories {1}-{2}'.format(threshold, col, original - len(u)))
+
+    data[numerical_feature_names] = data[numerical_feature_names].fillna(0, )
+
+    target = ['label']
+
+    numerical_features = [[x] for x in numerical_feature_names]
+    mms = MinMaxScaler(feature_range=(0, 1))
+    data[numerical_feature_names] = mms.fit_transform(data[numerical_feature_names]).astype(np.float32)
+    model_input = data[numerical_feature_names]
+
+    model = DeepFM(numerical_feature_names, cat_features, embedding_size=8)
+    print('training...', model.model_identity())
+    model.keras_model.compile(tf.keras.optimizers.Adam(1e-3), "binary_crossentropy",
+                  metrics=['binary_crossentropy'], )
+
+    tensorboard = TensorBoard(log_dir="logs/{0}".format(model.model_identity()))
+
+    train_model_cat_input = [data[feature].values for feature in cat_feature_names]
+    train_model_numerical_input = [data[feature].values for feature in numerical_feature_names]
+    train_target = data[target].values
+
+    input = train_model_cat_input + train_model_numerical_input
+
+    history = model.keras_model.fit(input, train_target,
+                        batch_size=256, epochs=10, verbose=2, validation_split=0.2, callbacks=[tensorboard])
