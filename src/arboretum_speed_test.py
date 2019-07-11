b037a4b75681283f40e32ff29bdf7c7dba5fae25
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-07-12 00:33:35 +0200

    extra parameters

--- src/arboretum_speed_test.py
+++ src/arboretum_speed_test.py
@@ -3,6 +3,7 @@ import pandas as pd
 import arboretum
 import json
 from sklearn.metrics import roc_auc_score
+from h2o4gpu.util.gpu import cudaresetdevice
 
 import time
 from sklearn.model_selection import train_test_split
@@ -10,61 +11,69 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     df = pd.read_csv('../data/HIGGS.csv', names=['label', 'lepton pT',
-                                                   'lepton eta', 'lepton phi', 'missing energy magnitude', 'missing energy phi',
-                                                   'jet 1 pt', 'jet 1 eta', 'jet 1 phi', 'jet 1 b-tag', 'jet 2 pt', 'jet 2 eta',
-                                                   'jet 2 phi', 'jet 2 b-tag', 'jet 3 pt', 'jet 3 eta', 'jet 3 phi', 'jet 3 b-tag',
-                                                   'jet 4 pt', 'jet 4 eta', 'jet 4 phi', 'jet 4 b-tag', 'm_jj', 'm_jjj', 'm_lv',
-                                                   'm_jlv', 'm_bb', 'm_wbb', 'm_wwbb'])
+                                                 'lepton eta', 'lepton phi', 'missing energy magnitude', 'missing energy phi',
+                                                 'jet 1 pt', 'jet 1 eta', 'jet 1 phi', 'jet 1 b-tag', 'jet 2 pt', 'jet 2 eta',
+                                                 'jet 2 phi', 'jet 2 b-tag', 'jet 3 pt', 'jet 3 eta', 'jet 3 phi', 'jet 3 b-tag',
+                                                 'jet 4 pt', 'jet 4 eta', 'jet 4 phi', 'jet 4 b-tag', 'm_jj', 'm_jjj', 'm_lv',
+                                                 'm_jlv', 'm_bb', 'm_wbb', 'm_wwbb'], nrows=1000000)
     labels = df['label'].values
     df = df.drop(['label'], axis=1)
     X = df.values
 
-    X_train, X_test, labels_train, labels_test = train_test_split(X, labels, test_size = 500000, random_state = 42)
+    for _ in range(1):
 
-    start_time = time.time()
+        start_time = time.time()
 
-    X_train = arboretum.DMatrix(X_train, y=labels_train)
-    X_test = arboretum.DMatrix(X_test)
+        X_train, X_test, labels_train, labels_test = train_test_split(
+            X, labels, test_size=0.2, random_state=42)
 
-    config = json.dumps({'objective': 1,
-                         'internals':
-{
-'double_precision': False,
-'compute_overlap': 3
-},
-                         'verbose':
-        {
-            'gpu': True,
-            'booster': True,
-            'data':True
-        },
-                         'tree':
+        X_train = arboretum.DMatrix(X_train, y=labels_train)
+        X_test = arboretum.DMatrix(X_test)
+
+        config = json.dumps({'objective': 1,
+                             'method': 1,
+                             'hist_size': 256,
+                             'internals':
+                             {
+                                 'double_precision': True,
+                                 'compute_overlap': 2,
+                                 'use_hist_subtraction_trick': True,
+                                 'dynamic_parallelism': False,
+                             },
+                             'verbose':
+                             {
+                                 'gpu': True,
+                                 'booster': True,
+                                 'data': False,
+                             },
+                             'tree':
                              {
                                  'eta': 0.1,
                                  'max_depth': 10,
                                  'gamma': 0.0,
                                  'min_child_weight': 100.0,
                                  'min_leaf_size': 0,
-                                 'colsample_bytree': 0.85,
-                                 'colsample_bylevel': 0.85,
+                                 'colsample_bytree': 1.0,
+                                 'colsample_bylevel': 1.0,
                                  'lambda': 0.0,
                                  'alpha': 0.0
                              }})
-    model = arboretum.Garden(config, X_train)
-    iter_time = time.time()
-    # grow trees
-    for i in range(100):
-        model.grow_tree()
-        print('next tree', time.time() - iter_time)
+        model = arboretum.Garden(config, X_train)
         iter_time = time.time()
-    print(time.time() -  start_time)
+        # grow trees
+        for i in range(2):
+            model.grow_tree()
+            print('next tree', time.time() - iter_time)
+            iter_time = time.time()
+        print(time.time() - start_time)
 
-    labels_pred = model.predict(X_train)
-    labels_pred_test = model.predict(X_test)
-    print('roc auc train: {0} test: {1}'.format(roc_auc_score(labels_train, labels_pred),
-                                                roc_auc_score(labels_test, labels_pred_test)))
+        labels_pred = model.predict(X_train)
+        labels_pred_test = model.predict(X_test)
+        print('roc auc train: {0} test: {1}'.format(roc_auc_score(labels_train, labels_pred),
+                                                    roc_auc_score(labels_test, labels_pred_test)))
 
+        del X_train
+        del X_test
+        del model
 
-
-
-
+        cudaresetdevice(0, -1)
--- src/xgb_hist_speed_test.py
+++ src/xgb_hist_speed_test.py
@@ -11,18 +11,17 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     df = pd.read_csv('../data/HIGGS.csv', names=['label', 'lepton pT',
-                                                   'lepton eta', 'lepton phi', 'missing energy magnitude', 'missing energy phi',
-                                                   'jet 1 pt', 'jet 1 eta', 'jet 1 phi', 'jet 1 b-tag', 'jet 2 pt', 'jet 2 eta',
-                                                   'jet 2 phi', 'jet 2 b-tag', 'jet 3 pt', 'jet 3 eta', 'jet 3 phi', 'jet 3 b-tag',
-                                                   'jet 4 pt', 'jet 4 eta', 'jet 4 phi', 'jet 4 b-tag', 'm_jj', 'm_jjj', 'm_lv',
-                                                   'm_jlv', 'm_bb', 'm_wbb', 'm_wwbb'])
+                                                 'lepton eta', 'lepton phi', 'missing energy magnitude', 'missing energy phi',
+                                                 'jet 1 pt', 'jet 1 eta', 'jet 1 phi', 'jet 1 b-tag', 'jet 2 pt', 'jet 2 eta',
+                                                 'jet 2 phi', 'jet 2 b-tag', 'jet 3 pt', 'jet 3 eta', 'jet 3 phi', 'jet 3 b-tag',
+                                                 'jet 4 pt', 'jet 4 eta', 'jet 4 phi', 'jet 4 b-tag', 'm_jj', 'm_jjj', 'm_lv',
+                                                 'm_jlv', 'm_bb', 'm_wbb', 'm_wwbb'], nrows=5000000)
     labels = df['label'].values
     df = df.drop(['label'], axis=1)
     X = df.values
 
-    X_train, X_test, labels_train, labels_test = train_test_split(X, labels, test_size = 500000, random_state = 42)
-
-    start_time = time.time()
+    X_train, X_test, labels_train, labels_test = train_test_split(
+        X, labels, test_size=0.2, random_state=42)
 
     X_train = xgboost.DMatrix(X_train, label=labels_train)
     X_test = xgboost.DMatrix(X_test)
@@ -31,23 +30,21 @@ if __name__ == '__main__':
              'silent': False, 'objective': "reg:logistic"}
     param['nthread'] = 4
     param['min_child_weight'] = 100
-    param['colspan_by_tree'] = 0.85
-    param['colspan_by_level'] = 0.85
+    param['colspan_by_tree'] = 1.0
+    param['colspan_by_level'] = 1.0
     param['lambda'] = 0.0
     param['eta'] = 0.1
     param['gamma'] = 0.0
     param['alpha'] = 0.0
     param['tree_method'] = 'gpu_hist'
 
+    # model = xgboost.train(param, X_train, 1)
+    start_time = time.time()
     model = xgboost.train(param, X_train, 100)
 
-    print(time.time() -  start_time)
+    print(time.time() - start_time)
 
     labels_pred = model.predict(X_train)
     labels_pred_test = model.predict(X_test)
     print('roc auc train: {0} test: {1}'.format(roc_auc_score(labels_train, labels_pred),
                                                 roc_auc_score(labels_test, labels_pred_test)))
-
-
-
-
