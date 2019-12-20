aeec597957e61da46ed55bf14ee5be2f0bc24cce
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-12-20 18:45:48 +0100

    download cretio

--- data/get_data.sh
+++ data/get_data.sh
@@ -2,3 +2,11 @@
 wget https://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz
 
 wget https://h2o-public-test-data.s3.amazonaws.com/h2o4gpu/open_data/allyears.1987.2013.zip
+
+wget azuremlsampleexperiments.blob.core.windows.net/criteo/day_0.gz
+wget azuremlsampleexperiments.blob.core.windows.net/criteo/day_1.gz
+wget azuremlsampleexperiments.blob.core.windows.net/criteo/day_2.gz
+wget azuremlsampleexperiments.blob.core.windows.net/criteo/day_3.gz
+wget azuremlsampleexperiments.blob.core.windows.net/criteo/day_4.gz
+wget azuremlsampleexperiments.blob.core.windows.net/criteo/day_5.gz
+wget azuremlsampleexperiments.blob.core.windows.net/criteo/day_6.gz
